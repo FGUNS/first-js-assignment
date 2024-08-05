14a87a44a43863f92ba671c9529799abd703537d
FGUNS <victorfreeze94@gmail.comgit config --global user.email victorfreeze@gmail.com>
2024-08-05 19:34:54 +0100

    first js assignment

---
+++ script.js
@@ -0,0 +1,40 @@
+let library= [];
+let library2=[];
+let library3=[];
+let library4=[];
+console.log(library);
+library.push({
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  yearPublished: 1925,
+  available: true
+});
+
+library.push({
+  title: "To Kill a Mockingbird",
+  author: "Harper Lee",
+  yearPublished: 1960,
+  available: true
+});
+
+library.push({
+  title: "1984",
+  author: "George Orwell",
+  yearPublished: 1949,
+  available: false
+});
+
+library.push({
+  title: "Pride and Prejudice",
+  author: "Jane Austen",
+  yearPublished: 1813,
+  available: true
+});
+// library.push({title:"The Great Gatsby"},{author:"F. Scott Fitzgerald"} ,{yearPublised:1925}, {avalaible:true} );
+// library2.push({title:"To Kill a Mockingbird"}, {author:"Harper lee"}, {yearPublised:1960},{available:true} );
+// library3.push({title:"1984"} , {author:"George Orwell"}, {yearPublised:1949},{available:false} );
+// library4.push({title:"pride and prejudice"} , {author:"Jane Austen "} , {yearPublised:1813} , {available:true} );
+console.log(library);
+console.log(library2);
+console.log(library3);
+console.log(library4);
