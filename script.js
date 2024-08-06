7b970f87bf6afe7239e157e3db25f24b6634ab88
FGUNS <victorfreeze94@gmail.comgit config --global user.email victorfreeze@gmail.com>
2024-08-06 12:12:51 +0100

    array

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 let library= [];
-let library2=[];
-let library3=[];
-let library4=[];
+
 console.log(library);
-library.push({
+let book1={
   title: "The Great Gatsby",
   author: "F. Scott Fitzgerald",
   yearPublished: 1925,
   available: true
-});
+};
 
-library.push({
+let book2={
   title: "To Kill a Mockingbird",
   author: "Harper Lee",
   yearPublished: 1960,
   available: true
-});
+};
 
-library.push({
+let book3 = {
   title: "1984",
   author: "George Orwell",
   yearPublished: 1949,
   available: false
-});
+};
 
-library.push({
+let book4 = {
   title: "Pride and Prejudice",
   author: "Jane Austen",
   yearPublished: 1813,
   available: true
-});
-// library.push({title:"The Great Gatsby"},{author:"F. Scott Fitzgerald"} ,{yearPublised:1925}, {avalaible:true} );
-// library2.push({title:"To Kill a Mockingbird"}, {author:"Harper lee"}, {yearPublised:1960},{available:true} );
-// library3.push({title:"1984"} , {author:"George Orwell"}, {yearPublised:1949},{available:false} );
-// library4.push({title:"pride and prejudice"} , {author:"Jane Austen "} , {yearPublised:1813} , {available:true} );
+};
+library.push(book1,book2,book3,book4 );
 console.log(library);
-console.log(library2);
-console.log(library3);
-console.log(library4);
