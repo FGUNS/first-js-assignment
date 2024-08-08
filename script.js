da0a83d2a5b9bc334ec2cef058f347221f28bea2
FGUNS <victorfreeze94@gmail.comgit config --global user.email victorfreeze@gmail.com>
2024-08-08 15:25:09 +0100

    update

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ let book4 = {
 };
 library.push(book1,book2,book3,book4 );
 console.log(library);
+
+book3.available = true
+console.log(book3);
+
+library.splice(3,1);
+console.log(library);
+
+for(let i= 0; i < library.length; 1++){
+    if(library[i].title.includes("The Great Gatsby")){
+        console.log("true and available");
+    }
+}
